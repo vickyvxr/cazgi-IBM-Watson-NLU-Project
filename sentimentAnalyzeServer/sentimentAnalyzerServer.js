f66262c72ad2b76e9a27c96e05333dcfd845cc00
vickyvxr <vickyvxr@gmail.com>
2021-06-06 06:35:20 +0000

    nlu-init

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -6,6 +6,25 @@ app.use(express.static('client'))
 const cors_app = require('cors');
 app.use(cors_app());
 
+function getNLUInstance(){
+    let api_key=process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url;
+    });
+
+    return naturalLanguageUnderstanding;
+}
+
+
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
